80e4fe35e28b760e1752ad8c561c3538c2e24de1
mayabs23-meet <109899548+mayabs23-meet@users.noreply.github.com>
2022-07-25 19:06:13 +0300

    i cant remove the first line

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,17 @@
-print("hello world")
+def create_youtube_video(title,description):
+	dict1={"title":title, "description":description,"like":0,"dislike":0,"comments":{}}
+	return dict1
+def like(dict1):
+	if "likes" in dict1:
+		dict1["likes"] += 1 
+	return dict1
+
+def add_comment(youtubevideo,username,comment_text):
+	youtubevideo["comments"][username] = comment_text
+	return youtubevideo 
+
+
+dict2 = create_youtube_video("bnana cake","plaplapal")
+for i in range (495):
+	like(dict2)
+print(dict2)
